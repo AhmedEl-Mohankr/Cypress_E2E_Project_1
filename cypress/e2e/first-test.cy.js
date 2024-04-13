de50c31799aeb68a81adbcf1b6caa428d4e1d18e
AhmedEl-Mohankr <96636877+AhmedEl-Mohankr@users.noreply.github.com>
2024-04-13 17:11:44 +0100

    Delete cypress directory

--- cypress/e2e/first-test.cy.js
+++
@@ -1,9 +0,0 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("http://localhost:5173/");
-
-    cy.get("li").should("have.length", 6);
-    cy.get("span").should("have.length", 12);
-  });
-});
-
